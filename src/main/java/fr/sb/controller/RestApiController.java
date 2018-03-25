93db0f1eddc3d03fce8e1a9d8c5b5f5a200032d7
mounir <mounirlyon@gmail.com>
2018-03-25 17:13:30 +0200

    Logger

--- src/main/java/fr/sb/controller/RestApiController.java
+++ src/main/java/fr/sb/controller/RestApiController.java
@@ -4,11 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 public class RestApiController {
 
+	private static final Logger logger = Logger.getLogger(RestApiController.class.getName());
+
 	@RequestMapping("/")
 	String home() {
+		logger.info("rest /");
 		return "Hello World!";
 	}
 
