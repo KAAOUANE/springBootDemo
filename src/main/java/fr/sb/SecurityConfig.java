224229677d3926688df4b539b218c6b0347edecf
mounir <mounirlyon@gmail.com>
2018-08-03 01:13:37 +0200

    spring security confi  in memory

---
+++ src/main/java/fr/sb/SecurityConfig.java
@@ -0,0 +1,24 @@
+package fr.sb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by admin on 03/08/2018.
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER").and()
+                .withUser("user2").password("password2").roles("USER2");
+    }
+}
+
+
