fb325f8a9077542e8545c9fff56307822614ebaf
mounir <mounirlyon@gmail.com>
2018-08-06 22:55:14 +0200

    add test Mapper

---
+++ src/test/mappers/CityMapperTest.java
@@ -0,0 +1,33 @@
+package mappers;
+
+import fr.sb.MyConfiguration;
+import fr.sb.bean.City;
+import fr.sb.mappers.CityMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes=MyConfiguration.class)
+public class CityMapperTest {
+
+    @Autowired
+    private CityMapper cityMapper;
+
+    @Test
+    public void findByStateTest() {
+        City city = cityMapper.findByState("CA");
+        assertThat(city.getName()).isEqualTo("San Francisco");
+        assertThat(city.getState()).isEqualTo("CA");
+        assertThat(city.getCountry()).isEqualTo("US");
+    }
+
+}
